75fa7ee894db6456d376f3951f10a3947f5f8fb0
Junsu Park <somadubel@gmail.com>
2021-04-05 20:27:22 +0900

    Update, Coroutine Scale Fade out

---
+++ Assets/Scenes/Coroutine/CoroutineScale.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoroutineScale : MonoBehaviour
+{
+    private Transform cubeTransform;
+    private Vector3 scaleChange;
+    
+    //1초동안 큐브가 사라지도록 만들기.
+    void Start()
+    {
+        cubeTransform = GetComponent<Transform>();
+        scaleChange = new Vector3(-0.1f, -0.1f, -0.1f);
+        StartCoroutine(RunFadeScale());
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    IEnumerator RunFadeScale()
+    {
+        while(cubeTransform.localScale.x > 0.0f)
+        {
+            cubeTransform.localScale += scaleChange;
+            Debug.Log(cubeTransform.localScale);
+            yield return new WaitForSeconds(0.1f); //0.01f면 0.1초만에 큐브가 사라진다.
+        }
+    }
+}
