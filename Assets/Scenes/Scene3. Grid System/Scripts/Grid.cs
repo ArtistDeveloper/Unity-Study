989afffed0da1023ec63c1a3c17212c2bee1f765
Junsu Park <somadubel@gmail.com>
2021-04-19 01:49:17 +0900

    comment edit

--- Assets/Scenes/Scene3. Grid System/Scripts/Grid.cs
+++ Assets/Scenes/Scene3. Grid System/Scripts/Grid.cs
@@ -17,15 +17,13 @@ public class Grid
         this.cellSize = cellSize; //셀 사이즈를 통해 각 인덱스를 땅위에 올리기 위한 WorldPosition을 계산할 수 있다.
 
         gridArray = new int[width, height];
-        // 제대로 Grid가 생성되는지 테스트
-        // Debug.Log(width + " " + height);
+        // Debug.Log(width + " " + height); // 제대로 Grid가 생성되는지 테스트
 
         for (int x = 0; x < gridArray.GetLength(0); x++)
         {
             for (int y = 0; y < gridArray.GetLength(1); y++) 
             {
-                // grid배열의 값 확인
-                // Debug.Log(x + ", " + y);
+                // Debug.Log(x + ", " + y); // grid배열의 값 확인
 
                 // 첫 파라미터 : gridArray[x, y] => x행 y열의 값을 가져와서 String으로 바꿔준다.
                 UtilsClass.CreateWorldText(gridArray[x, y].ToString(), null, GetWorldPosition(x, y), 20, Color.white, TextAnchor.MiddleCenter);
