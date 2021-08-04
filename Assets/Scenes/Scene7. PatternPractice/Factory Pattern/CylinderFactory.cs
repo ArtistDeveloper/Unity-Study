ede8c16c3203b74cec51f81ff786ec8231382655
Artiper <somadubel@gmail.com>
2021-08-05 02:24:08 +0900

    ADD : Factory Pattern Practice (1)

---
+++ Assets/Scenes/Scene7. PatternPractice/Factory Pattern/CylinderFactory.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CylinderFactory : AbsFigureFactory
+{
+    public override Figure CreateFigure()
+    {
+        Figure implement = null;
+        return implement;
+    }
+}
---
+++ Assets/Scenes/Scene7. PatternPractice/Factory Pattern/SphereFactory.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereFactory : AbsFigureFactory
+{
+    public RedSphere redSphere;
+    public BlueSphere blueSphere;
+
+    public override Figure CreateFigure()
+    {
+        Figure implement = null;
+        return implement;
+    }
+}
