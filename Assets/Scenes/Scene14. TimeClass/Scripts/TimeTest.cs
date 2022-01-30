2d3ce6f59444aeae9857badd3ac1d88e8bfeb886
Artiper <somadubel@gmail.com>
2022-01-31 03:24:58 +0900

    UPDATE : TimeClass 업데이트

--- Assets/Scenes/Scene14. TimeClass/Scripts/TimeTest.cs
+++ Assets/Scenes/Scene14. TimeClass/Scripts/TimeTest.cs
@@ -2,15 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TimeTest : MonoBehaviour
+namespace Scene14
 {
-    private void Start()
+    public class TimeTest : MonoBehaviour
     {
-        Application.targetFrameRate = 30;
-    }
+        private float distancePerSecond;
 
-    private void Update()
-    {
-        Debug.Log(Time.deltaTime);
+        private void Start()
+        {
+            distancePerSecond = 1f;
+            Application.targetFrameRate = 30;
+            // Application.targetFrameRate = 60;
+
+            Debug.Log($"스칼라 곱 구하기 : {Vector3.forward * 2.5f}"); // 0.03을 곱했을 떄, 0,0,0으로 나와서 이상했는데 그냥 0이하 소수점 자리 출력이 안됐었던 것 같다.
+        }
+
+        private void Update()
+        {
+            // 프레임에 따라 캐릭터가 움직이는 속도가 달라진다.
+            if (Input.GetKey(KeyCode.W))
+            {
+                transform.position = transform.position + Vector3.forward * Time.deltaTime;
+                // transform.Translate(0, 0, distancePerSecond * Time.deltaTime);
+            }
+        }
     }
 }
